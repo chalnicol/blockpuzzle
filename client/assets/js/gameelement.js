cf20f31ff23c4da12ce5c9beb95b3d552c563906
chalnicol <chalnicol@gmail.com>
2021-03-17 21:43:28 +0800

    Almost

--- client/assets/js/gameelement.js
+++ client/assets/js/gameelement.js
@@ -1,6 +1,6 @@
 class GameElement extends Phaser.GameObjects.Container {
 
-    constructor(scene, x, y, children, dataObj, dims, scl=1 ) {
+    constructor(scene, x, y, children, dataObj, dims, clrid, scl=1 ) {
 
         super(scene, x, y, children);
         // ...
@@ -14,9 +14,31 @@ class GameElement extends Phaser.GameObjects.Container {
 
         this.dataObj = dataObj;
 
-        this.scl = scl;
+        this.clrid = clrid;
 
+        this.scl = scl;
        
+        //const randClrNmbr = Math.floor ( Math.random() * 3 );
+
+        let clr = 0;
+
+        switch ( clrid ) {
+            case 0:
+                clr = 0x33ff33;
+                break;
+            case 1:
+                clr = 0x33ffff;
+                break;
+            case 2:
+                clr = 0xff33ff;
+                break;
+            default:
+                //clr = '0xffff00';
+                break;
+        }
+
+        this.myClr = clr;
+
         //...
 
         for ( var i = 0; i < this.dataObj.row ; i++ ) {
@@ -25,22 +47,20 @@ class GameElement extends Phaser.GameObjects.Container {
 
                 if ( this.dataObj.arr [i][j] == 1 ) {
 
-                    let rct = this.scene.add.rectangle ( ( j * dims) - wd/2 + (dims/2) , ( i *dims ) - ht/2 + (dims/2),  dims, dims, 0xffff00, 1 ).setStrokeStyle (2, 0x000000).setScale(scl);
+                    let rct = this.scene.add.rectangle ( ( j * dims) - wd/2 + (dims/2) , ( i *dims ) - ht/2 + (dims/2),  dims, dims, clr, 1 ).setStrokeStyle (2, 0x000000).setScale(scl);
 
                     this.add ( rct );
 
                 }
     
             }
-0
+
         }
-
-        // const p = this.scene.add.rectangle (0, 0, 20, 20, 0xff33ff, 1);
-
-        // this.add (p);
 
         scene.add.existing(this);
 
     }
+
+
     
 }
--- client/assets/js/intro.js
+++ client/assets/js/intro.js
@@ -18,21 +18,29 @@ class Intro extends Phaser.Scene {
         // create grid 
         var _this = this;
 
-        this.round = 0;
-
         this.counter = 0;
 
         this.boxClicked = 0;
 
-        this.eldata = 0;
+        //this.eldata = 0;
 
         this.cellCollided = -1;
+
+        this.grid = [];
+
+        this.elementsData = [];
+
+        this.score = 0;
 
         this.isBoxClicked = false;
 
         let _gW = this.game.config.width,
             _gH = this.game.config.height;
 
+
+        this.scoreTxt = this.add.text ( 10, 10, 'Score : 0', { fontSize: _gH*0.03, fontFamily:'Oswald', color:'black' });
+
+        //create grid..
         const bsize = (_gW*0.96)/8;
 
         const sx = (_gW - (_gW*0.96))/2, 
@@ -48,19 +56,21 @@ class Intro extends Phaser.Scene {
             let xp = iy * ( bsize ) + sx,
                 yp = ix * ( bsize ) + sy;
 
-            let rcta = this.add.rectangle (xp, yp, bsize, bsize, 0xff3333, 1 ).setOrigin (0).setStrokeStyle( 2, 0xffffff ).setData ('id', i ).setName('cell'+i);
+            let rcta = this.add.rectangle (xp, yp, bsize, bsize, 0x8e8e8e, 1 ).setOrigin (0).setStrokeStyle( 2, 0xffffff ).setData ('id', i ).setName('cell'+i);
 
             let txt = this.add.text (xp +(bsize*0.1), yp + (bsize*0.05), i, { color:'white', fontSize : bsize*0.3, fontFamily : 'Oswald'} );
 
             this.cellsCont.add ( [rcta, txt] );
+
+            this.grid.push ( 0 );
 
         }
 
         this.trueSize = bsize;
 
 
-        // create 3 boxes
 
+        //create 3 boxes..
         this.bottomCont = this.add.container (0, 0)
 
         const csize = _gW * 0.3,
@@ -71,16 +81,13 @@ class Intro extends Phaser.Scene {
 
         for ( var i = 0; i < 3; i++ ) {
 
-            let box = this.add.rectangle ( i * (csize+cspace) + sxa,  sya, csize, csize, 0xff3333, 1 ).setName ('box' + i ).setData('id', i);
+            let box = this.add.rectangle ( i * (csize+cspace) + sxa,  sya, csize, csize, 0x3a3a3a, 1 ).setName ('box' + i ).setData('id', i);
 
             box.on ('pointerdown', function ( pointer ) {
                 
-                let boxClicked = this.getData('id');;
+                _this.boxClicked = this.getData('id');
 
-               _this.popElements (pointer, boxClicked);
-
-               _this.boxClicked = boxClicked;
-
+               _this.popElements (pointer);
 
             });
 
@@ -89,9 +96,6 @@ class Intro extends Phaser.Scene {
         }
 
         // load elements data.. 
-        
-        this.elementsCont = this.add.container (0, 0);
-
         this.elements = [];
 
         var xmlhttp = new XMLHttpRequest();
@@ -107,22 +111,28 @@ class Intro extends Phaser.Scene {
         xmlhttp.open("GET", "client/assets/json/data.json", true);
         xmlhttp.send();
 
-        
-        //..
+        //create container for elements..
+        this.elementsCont = this.add.container (0, 0);
 
+        //create container for cells fielded..
+        this.cellPermanentCont = this.add.container(0, 0);
+
+        //..
         this.input.on ('pointerup', function ( pointer ) {
 
             if ( this.isBoxClicked ) {
 
                
                 if ( this.cellCollided >= 0 ) {
+                    
                     this.setPermanent ();
+
                 }else {
                     this.retract ();
+
+                    this.clearCells ();
                 }
                 
-                this.clearCells ();
-
                 this.isBoxClicked = false;
 
             }
@@ -140,19 +150,25 @@ class Intro extends Phaser.Scene {
     createRandomElements () 
     {
         
+        this.elementsData = [];
+
         for ( var i = 0; i < 3; i++ ) {
 
             let box = this.bottomCont.getByName ('box' + i );
 
+            box.setInteractive()
+
             let randNum = Math.floor ( Math.random() * this.elements.length );
+
+            let rndClrId = Math.floor ( Math.random() * 3 );
 
             let dim = box.width * 0.9/5;
 
-            let element = new GameElement ( this, box.x, box.y, null,  this.elements [ randNum ], dim, 1 ).setName ('el' + i );
+            let element = new GameElement ( this, box.x, box.y, null,  this.elements [ randNum ], dim, rndClrId, 1 ).setName ('el' + i );
         
             this.elementsCont.add (element);
 
-            box.setInteractive().setData('eldata', randNum);
+            this.elementsData.push ( { 'style' : randNum, 'colorid' : rndClrId } );
 
         }
 
@@ -160,23 +176,30 @@ class Intro extends Phaser.Scene {
 
     }
 
-    popElements ( pointer, id ) 
+    popElements ( pointer ) 
     {   
+
+        const edata = this.elementsData [ this.boxClicked ].style;
+
+        const eclr = this.elementsData [ this.boxClicked ].colorid;
 
         this.isBoxClicked = true;
 
         //hide element..
-        this.elementsCont.getByName ('el' + id ).visible = false;
+        this.elementsCont.getByName ('el' + this.boxClicked ).visible = false;
 
         //create bigger element..
 
-        let box = this.bottomCont.getByName ('box' + id );
-        
-        this.eldata = box.getData('eldata') ;
+        let box = this.bottomCont.getByName ('box' + this.boxClicked );
 
-        let totalH = this.elements [this.eldata].row * this.trueSize;
+        box.disableInteractive();
 
-        let bigger = new GameElement ( this, box.x, box.y, null,  this.elements [ this.eldata ], this.trueSize, 0.9 ).setName ('bigEl' + id );
+
+        //..
+
+        let totalH = this.elements [edata].row * this.trueSize;
+
+        let bigger = new GameElement ( this, box.x, box.y, null,  this.elements [ edata ], this.trueSize, eclr, 0.9 ).setName ('bigEl' + this.boxClicked );
 
         bigger.setScale ( 0.7 );
 
@@ -187,9 +210,6 @@ class Intro extends Phaser.Scene {
             duration : 30,
             ease : 'Linear'
         });
-
-        box.disableInteractive();
-
 
 
     }
@@ -230,47 +250,59 @@ class Intro extends Phaser.Scene {
         
         this.clearCells ();
 
+        this.resetLines();
+
         let box = this.bottomCont.getByName ('box' + this.boxClicked );
         
-        let totalH = this.elements [ box.getData('eldata') ].row * this.trueSize;
+        let totalH = this.elements [ this.elementsData [this.boxClicked].style ].row * this.trueSize;
 
         let bigEl = this.children.getByName ( 'bigEl' + this.boxClicked );
 
         bigEl.setPosition ( x, y - (totalH/2)  );
 
 
-        for ( var i = 0; i < 64; i++) {
+        let cellHit = this.getHit ();
 
-        
-            let cell = this.cellsCont.getByName ('cell' + i );
+        if ( cellHit != null ) {
 
-            if ( this.hitTest ( bigEl, cell) ) {
+            if ( this.checkField ( cellHit ) ) {
 
-                if ( this.checkField (i) ) {
+                this.cellCollided = cellHit;
 
-                    this.cellCollided = i;
+                this.illuminate ( cellHit );
 
-                    this.illuminate ( i );
-
-                }else {
-
-                    this.cellCollided = -1;
-                }
-                
+                this.tempCheckLines ();
 
             }
-
-        }   
+            
+        }
        
     }
 
     hitTest ( el, cell ) 
-    
     {
         return (el.x - el.width/2) >= (cell.x - cell.width/2) 
             && (el.y - el.height/2) >= (cell.y - cell.height/2)
             && (el.x - el.width/2) < ( cell.x + cell.width/2)
             && (el.y - el.height/2) < ( cell.y + cell.height/2);
+
+    }
+
+    getHit () {
+
+        let bigEl = this.children.getByName ( 'bigEl' + this.boxClicked );
+
+        for ( var i = 0; i < 64; i++) {
+        
+            let cell = this.cellsCont.getByName ('cell' + i );
+
+            if ( this.hitTest ( bigEl, cell )) {
+                return i;
+            }
+
+        }   
+
+        return null;
 
     }
 
@@ -282,7 +314,7 @@ class Intro extends Phaser.Scene {
 
             let cell = this.cellsCont.getByName ('cell' + i );
 
-            cell.setFillStyle ( 0xff3333, 1);
+            cell.setFillStyle (0x8e8e8e, 1);
             
         }   
 
@@ -291,7 +323,7 @@ class Intro extends Phaser.Scene {
 
     illuminate ( cellid ) {
 
-        const mydata = this.elements [ this.eldata ];
+        const mydata = this.elements [ this.elementsData [ this.boxClicked ].style ];
 
         const r = Math.floor ( cellid/8 ), c = cellid % 8;
          
@@ -307,7 +339,7 @@ class Intro extends Phaser.Scene {
                         
                         let cell = this.cellsCont.getByName ( 'cell' + n );
 
-                        cell.setFillStyle (0xffcc55, 1);
+                        cell.setFillStyle (0xcecece, 1);
 
                     }
                     
@@ -320,7 +352,7 @@ class Intro extends Phaser.Scene {
 
     checkField ( cellid ) {
 
-        const mydata = this.elements [ this.eldata ];
+        const mydata = this.elements [ this.elementsData [ this.boxClicked ].style  ];
 
         const r = Math.floor ( cellid/8 ), c = cellid % 8;
          
@@ -330,6 +362,14 @@ class Intro extends Phaser.Scene {
 
                 if ( (i + r) >= 8 || (c+j) >= 8  ) return false;
 
+                if ( mydata.arr [i][j] == 1 ) {
+
+                    let cellid = ( i+r) * 8 + ( c+j );
+
+                    if ( this.grid [ cellid ] == 1 ) return false;
+
+                }
+               
             }
 
         }
@@ -339,41 +379,317 @@ class Intro extends Phaser.Scene {
     }
 
     setPermanent ()
-    {
-        let _this =
+    {   
 
-        let cell = this.cellsCont.getByName ('cell' + this.cellCollided );
+        let _this = this;
 
-        let bigEl = this.children.getByName ( 'bigEl' + this.boxClicked );
+        let cll = this.cellsCont.getByName ('cell' + this.cellCollided);
 
-        const bx = cell.x + bigEl.width/2,
-              by = cell.y + bigEl.height/2;
-        
+        let bigEl = this.children.getByName ('bigEl' + this.boxClicked);
+
+        const bx = cll.x + bigEl.width/2,
+              by = cll.y + bigEl.height/2;
+
         this.add.tween ({
             targets : bigEl,
             x : bx, y : by,
             duration : 30,
             ease : 'Linear',
             onComplete: function () {
-                
-                
+                bigEl.destroy ();
+                _this.showPermanent ();
             }
+        });
+
+        bigEl.iterate ( function ( child ) {
+            _this.add.tween ({
+                targets : child,
+                scaleX : 1, scaleY : 1,
+                duration : 30,
+                ease : 'Linear'  
+            })
         });
 
         //destroy        
         this.elementsCont.getByName ('el' + this.boxClicked ).destroy();
-
-        //create elements after 3
-        this.counter += 1;
-
-        if ( this.counter >= 3 ) {
-            this.createRandomElements ();
-        }
-        
-        
     }
 
+    showPermanent () 
+    {
 
-    
+        const mydata = this.elements [ this.elementsData [ this.boxClicked ].style ];
+
+        const r = Math.floor ( this.cellCollided/8 ), c = this.cellCollided % 8;
+
+        let clr = 0;
+
+        let clrid = this.elementsData [ this.boxClicked ].colorid;
+
+        switch ( clrid ) {
+            case 0:
+                clr = 0x33ff33;
+                break;
+            case 1:
+                clr = 0x33ffff;
+                break;
+            case 2:
+                clr = 0xff33ff;
+                break;
+            default:
+                //clr = '0xffff00';
+                break;
+        }
+
+
+        for ( var i = 0; i < mydata.row ; i++ ) {
+
+            for ( var j = 0; j < mydata.col ; j++ ) {
+
+                if ( mydata.arr [i][j] == 1 ){
+
+                    let cellid = (( i+r ) * 8)  +  ( c+j );
+
+                    let cll = this.cellsCont.getByName ('cell' + cellid );
+
+                    let cellP = this.add.rectangle ( cll.x + this.trueSize/2, cll.y + this.trueSize/2, this.trueSize, this.trueSize, clr, 1 ).setStrokeStyle ( 2, 0x1a1a1a ).setName ('cp' + cellid ).setData('origClr', clrid );
+
+                    this.cellPermanentCont.add ( cellP );
+
+                    this.grid [ cellid ] = 1;
+
+                    this.score += 1;
+                }
+
+            }
+
+        }
+
+        this.clearCells ();
+
+        this.checkLines ();
+
+        this.counter += 1;
+        if ( this.counter >= 3 ) {
+            this.counter = 0;
+            this.createRandomElements ();
+        }
+
+
+    }
+
+    getLines ( mygrid ) 
+    {
+
+        let fin = [];
+
+        let lines = 0;
+
+        //check by rows..
+        for ( var i = 0; i < 8; i++ ) {
+
+            let myCounter = 0;
+
+            for ( var j = 0; j < 8; j++ ) {
+                if ( mygrid [ (i*8)+j ] == 1 ) {
+                    myCounter += 1;
+                }
+            }
+
+            if ( myCounter >= 8 ) {
+                lines += 1 ;
+                myCounter = 0;
+
+                for ( var k = 0; k < 8; k++ ) {
+                    fin.push (  (i*8)+k );
+                }
+
+            }
+            
+        }
+
+        //check by cols 
+        for ( var j = 0; j < 8; j++ ) {
+
+            let myCounter = 0;
+
+            for ( var i = 0; i < 8; i++ ) {
+
+                if ( mygrid [ (i*8)+j ] == 1 ) {
+                    myCounter += 1;
+                }
+
+            }
+
+            if ( myCounter >= 8 ) {
+
+                lines += 1 ;
+                myCounter = 0;
+
+                for ( var k = 0; k < 8; k++ ) {
+                    if ( !fin.includes ( (k*8)+j ) ) {
+                        fin.push ( (k*8)+j );
+                    }
+                }
+
+            }
+            
+        }
+
+        return { 'fin' : fin, 'lines' : lines }
+
+    }
+
+    checkLines () {
+
+        let linesdata = this.getLines ( this.grid );
+
+        if ( linesdata.lines == 1 ) {
+            this.score += 10;
+        }else if ( linesdata.lines == 2 ) {
+            this.score += 30;
+        }else if ( linesdata.lines == 3 ) {
+            this.score += 60;
+        }else if ( linesdata.lines == 4 ) {
+            this.score += 100;
+        }else if ( linesdata.lines > 4 ) {
+            this.score += 200;
+        }
+        
+        for ( var i = 0; i < linesdata.fin.length; i++ ) {
+
+            let cell = this.cellPermanentCont.getByName ('cp' + linesdata.fin[i] );
+
+            this.add.tween ({
+                targets : cell,
+                scaleX : 0.5, scaleY : 0.5,
+                duration : 80,
+                yoyo : true,
+                repeat : 2,
+                ease : 'Linear',
+                onComplete : function () {
+                    this.targets [0].destroy ();
+                }
+            });
+
+            this.grid [ linesdata.fin[i] ] = 0;
+        }
+
+        this.scoreTxt.text = 'Score : ' + this.score;
+
+    }
+
+    tempCheckLines ()
+    {
+
+        let tempGrid = this.getTempGrid ();
+
+        const mydata = this.elements [ this.elementsData [ this.boxClicked ].style ];
+
+        const r = Math.floor ( this.cellCollided/8 ), c = this.cellCollided % 8;
+
+        for ( var i = 0; i < mydata.row ; i++ ) {
+
+            for ( var j = 0; j < mydata.col ; j++ ) {
+
+                if ( mydata.arr [i][j] == 1 ){
+
+                    let cellid = (( i+r ) * 8)  +  ( c+j );
+
+                    tempGrid [ cellid ] = 1;
+                }
+
+            }
+
+        }
+
+        let linesdata = this.getLines ( tempGrid );
+
+        //console.log ( 'lines',  linesdata.lines );
+
+        if ( linesdata.lines > 0 ) {
+
+            let clr = 0;
+
+            switch ( this.elementsData [ this.boxClicked ].colorid ) {
+                case 0:
+                    clr = 0x33ff33;
+                    break;
+                case 1:
+                    clr = 0x33ffff;
+                    break;
+                case 2:
+                    clr = 0xff33ff;
+                    break;
+                default:
+                    //clr = '0xffff00';
+                    break;
+            }
+
+
+            for ( var i = 0; i < linesdata.fin.length; i++ ) {
+
+                if ( this.grid [ linesdata.fin[i]  ] == 1 ){
+
+                    let cell = this.cellPermanentCont.getByName ('cp' + linesdata.fin[i] );
+
+                    cell.setFillStyle ( clr, 1 );
+                }
+
+            }
+
+        }
+
+    }
+
+    resetLines () {
+
+        this.cellPermanentCont.iterate ( function ( child ) {
+            
+            let clr = 0;
+
+            //console.log ( child.getData ('origClr')  );
+
+            switch ( child.getData ('origClr')) {
+                case 0:
+                    clr = 0x33ff33;
+                    break;
+                case 1:
+                    clr = 0x33ffff;
+                    break;
+                case 2:
+                    clr = 0xff33ff;
+                    break;
+                default:
+                    //clr = '0xffff00';
+                    break;
+            }
+
+            child.setFillStyle (clr, 1 );
+
+
+
+        });
+
+    }
+
+    getTempGrid () {
+
+        let arr = [];
+
+        for ( var i = 0; i < this.grid.length; i++ ) {
+            arr.push ( this.grid[i] );
+        }
+
+        return arr;
+
+    }
+
+    checkFit () 
+    {
+
+        
+
+    }
+
 
 }
